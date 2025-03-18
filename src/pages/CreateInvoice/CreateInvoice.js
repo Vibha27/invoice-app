10e036f20a9352120a897bb8ae476af53f9a0429
Vibha27 <svibha081@gmail.com>
2025-03-19 01:18:05 +0530

    Added invoice object with form containing invoice nunber, client name, note and tax number along with action buttons and its  styling

--- src/pages/CreateInvoice/CreateInvoice.js
+++ src/pages/CreateInvoice/CreateInvoice.js
@@ -1,8 +1,71 @@
-import React from 'react';
-import { Wrapper } from './style';
+import React, { useState } from "react";
+import { InvoiceAction, InvoiceForm, Wrapper } from "./style";
+import InputField from "../../components/InputField";
+import Button from "../../components/Button";
 
 export const CreateInvoice = () => {
-    return <Wrapper>
-        <h2>Create Invoices</h2>
+  const [invoiceDetail, setInvoiceDetail] = useState({
+    invoiceNumber: "",
+    clientName: "",
+    taxNumber: "",
+    note: "",
+    items: [
+        {
+            laborCharge: 0,
+            materialCost: 0,
+            hourlyRate: 0,
+            hourWorked: 0,
+            workExpense: 0
+        }
+    ]
+  });
+
+  const handleInputChange = () => {
+    return
+  }
+
+  return (
+    <Wrapper>
+      <h2>Create Invoice</h2>
+      <hr />
+      <InvoiceForm style={{ padding: "20px" }}>
+        <InputField
+          label="Invoice Number"
+          type="text"
+          placeholder="eg: IA00001"
+          value={invoiceDetail.invoiceNumber}
+          onChange={(e) => handleInputChange(e.target.value)}
+          required
+        />
+
+        <InputField
+          label="Client"
+          type="text"
+          placeholder="Enter client name"
+          value={invoiceDetail.clientName}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+
+        <InputField
+          label="Tax Number"
+          type="text"
+          placeholder="11111111111"
+          value={invoiceDetail.taxNumber}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+
+        <InputField
+          label="Note"
+          type="textarea"
+          placeholder="Short note on how to proceed futher with this invoice"
+          value={invoiceDetail.note}
+          onChange={(e) => handleInputChange(e.target.value)}
+        />
+      </InvoiceForm>
+      <InvoiceAction>
+        <Button variant="secondary">Cancel</Button>&nbsp;
+        <Button variant="primary">Save Invoice</Button>
+      </InvoiceAction>
     </Wrapper>
-}
+  );
+};
--- src/pages/CreateInvoice/style.js
+++ src/pages/CreateInvoice/style.js
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    background-color: white;
-    height: calc(100vh - 90px);
-    overflow-y: auto;
-    overflow-x: hidden;
-    border-radius: 12px;
-    padding: 4px 18px;
+  background-color: white;
+  height: calc(100vh - 90px);
+  overflow-y: auto;
+  overflow-x: hidden;
+  border-radius: 12px;
+  padding: 4px 18px;
 `;
 
-export {
-    Wrapper
-}
+const InvoiceForm = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 30px;
+`;
+
+const InvoiceAction = styled.div`
+    display: flex;
+    justify-content: flex-end;
+
+`;
+
+export { Wrapper, InvoiceForm, InvoiceAction };
